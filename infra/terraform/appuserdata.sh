fe7bde7b617147d1bc40b6b56a5378ba25eca5da
sgrsaga <sgrsagara@gmail.com>
2025-12-16 08:54:36 +0530

    Add Fluentd Dockerfile and update userdata script for Fluentd setup

--- infra/terraform/appuserdata.sh
+++ infra/terraform/appuserdata.sh
@@ -29,5 +29,11 @@ docker compose up -d
 docker run -d --name=node-exporter -p 9100:9100 prom/node-exporter
 
 ## Setup fluentd package 6
-curl -fsSL https://fluentd.cdn.cncf.io/sh/install-ubuntu-noble-fluent-package6-lts.sh | sh
-
+#curl -fsSL https://fluentd.cdn.cncf.io/sh/install-ubuntu-noble-fluent-package6-lts.sh | sh
+docker run -d --name=fluentd -p 24224:24224 -p 24224:24224/udp \
+-v /data:/fluentd/log \
+-v /data/fluentd/etc/fluent.conf:/fluentd/etc/fluent.conf:rw \
+-v /data/fluentd/plugins:/fluentd/plugins \
+-v /data/fluentd/certs:/fluentd/certs \
+-v /var/logs:/var/logs:rw \
+fluent/fluentd:edge-debian
